0443b383678bf41a1ddd6679996887aa77a1990d
wanglikuan <897074310@qq.com>
2022-01-26 13:57:27 +0800

    0126-new-cifar

---
+++ generate_cifar100.py
@@ -0,0 +1,99 @@
+import numpy as np
+import os
+import sys
+import random
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from utils.dataset_utils import check, separate_data, split_data, save_file
+
+
+random.seed(1)
+np.random.seed(1)
+num_clients = 20
+num_labels = 10
+dir_path = "Cifar100-alpha0.05/"
+
+
+def sparse2coarse(targets):
+    """Convert Pytorch CIFAR100 sparse targets to coarse targets.
+    Usage:
+        trainset = torchvision.datasets.CIFAR100(path)
+        trainset.targets = sparse2coarse(trainset.targets)
+    """
+    coarse_labels = np.array([ 4,  1, 14,  8,  0,  6,  7,  7, 18,  3,  
+                               3, 14,  9, 18,  7, 11,  3,  9,  7, 11,
+                               6, 11,  5, 10,  7,  6, 13, 15,  3, 15,  
+                               0, 11,  1, 10, 12, 14, 16,  9, 11,  5, 
+                               5, 19,  8,  8, 15, 13, 14, 17, 18, 10, 
+                               16, 4, 17,  4,  2,  0, 17,  4, 18, 17, 
+                               10, 3,  2, 12, 12, 16, 12,  1,  9, 19,  
+                               2, 10,  0,  1, 16, 12,  9, 13, 15, 13, 
+                              16, 19,  2,  4,  6, 19,  5,  5,  8, 19, 
+                              18,  1,  2, 15,  6,  0, 17,  8, 14, 13])
+    return coarse_labels[targets]
+
+
+# Allocate data to users
+def generate_cifar100(dir_path, num_clients, num_labels, niid=False, real=True, partition=None):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        
+    # Setup directory for train/test data
+    config_path = dir_path + "config.json"
+    train_path = dir_path + "train/train.json"
+    test_path = dir_path + "test/test.json"
+
+    if check(config_path, train_path, test_path, num_clients, num_labels, niid, real, partition):
+        return
+        
+    # Get Cifar100 data
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR100(
+        root="cifar100_rawdata", train=True, download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR100(
+        root="cifar100_rawdata", train=False, download=True, transform=transform)
+
+    trainset.targets = sparse2coarse(trainset.targets) # cifar100  sparse2coarse
+    testset.targets = sparse2coarse(testset.targets) # cifar100  sparse2coarse
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=len(trainset.data), shuffle=False)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=len(testset.data), shuffle=False)
+
+    for _, train_data in enumerate(trainloader, 0):
+        trainset.data, trainset.targets = train_data
+    for _, test_data in enumerate(testloader, 0):
+        testset.data, testset.targets = test_data
+
+    dataset_image = []
+    dataset_label = []
+
+    dataset_image.extend(trainset.data.cpu().detach().numpy())
+    dataset_image.extend(testset.data.cpu().detach().numpy())
+    dataset_label.extend(trainset.targets.cpu().detach().numpy())
+    dataset_label.extend(testset.targets.cpu().detach().numpy())
+    dataset_image = np.array(dataset_image)
+    dataset_label = np.array(dataset_label)
+
+    # dataset = []
+    # for i in range(num_labels):
+    #     idx = dataset_label == i
+    #     dataset.append(dataset_image[idx])
+
+    X, y, statistic = separate_data((dataset_image, dataset_label), num_clients, num_labels, 
+                                    niid, real, partition, class_per_client=20)
+    train_data, test_data = split_data(X, y)
+    save_file(config_path, train_path, test_path, train_data, test_data, num_clients, num_labels, 
+        statistic, niid, real, partition)
+
+
+if __name__ == "__main__":
+    niid = True if sys.argv[1] == "noniid" else False
+    real = True if sys.argv[2] == "realworld" else False
+    partition = sys.argv[3] if sys.argv[3] != "-" else None
+
+    generate_cifar100(dir_path, num_clients, num_labels, niid, real, partition)
